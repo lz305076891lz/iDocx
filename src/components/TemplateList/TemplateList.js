3c372a07cb1ea6c8f0ad1dd0c71f31ceeb40a947
Mondo <mondogao@gmail.com>
2017-02-27 15:36:19 +0800

    feat: add Search for Template

--- src/components/TemplateList/TemplateList.js
+++ src/components/TemplateList/TemplateList.js
@@ -3,12 +3,30 @@ import React from 'react';
 import styles from  './TemplateList.css';
 
 export default class TemplateList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      searchValue: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className={styles.template}>
         <div className="container">
-          <div>搜索框</div>
-          <div>列表</div>
+          <div className={styles.search}>
+            {/*<h3>选择模版</h3>*/}
+            <input name="search" className="shadow-box" type="text" value={this.state.searchValue} onChange={this.handleChange} placeholder="输入学校或专业搜索"/>
+          </div>
+          <div>{this.state.searchValue}</div>
         </div>
       </div>
     );
