359c8f42ba39d7578eccb17b7c95a91399d8ce7b
Mondo <mondogao@gmail.com>
2017-03-18 13:56:05 +0800

    feat(components/headernav): 增加登陆表单

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,19 +1,99 @@
 import React from 'react'
-import { Menu } from 'antd'
+import { Button, Menu, Row, Col, Modal, Form, Input, Radio } from 'antd'
 import Container from 'components/ResponsiveContainer'
-
 import styles from  './HeaderNav.pcss'
+
+const FormItem = Form.Item
 
 const HeaderNav = ({menuData}) => (
   <Container isAbsolute={true} style={{zIndex: 10}}>
-    <Menu
-      mode="horizontal"
-      defaultSelectedKeys={['1']}
-      className={styles.menu}
-    >
-      {menuData.map((item) => <Menu.Item key={item.id}>{item.title}</Menu.Item>)}
-    </Menu>
+    <Row>
+      <Col xs={20}>
+        <Menu
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          className={styles.menu}
+        >
+          {menuData.map((item) => <Menu.Item key={item.id}>{item.title}</Menu.Item>)}
+        </Menu>
+      </Col>
+      <Col xs={4}>
+        <SignInOut/>
+      </Col>
+    </Row>
   </Container>
+)
+
+class SignInOut extends React.Component{
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoginVisible: true
+    }
+
+    this.handleLoginCancel = this.handleLoginCancel.bind(this)
+    this.showLoginModel = this.showLoginModel.bind(this)
+  }
+
+  handleLoginCancel () {
+    this.setState({
+      isLoginVisible: false
+    })
+  }
+
+  showLoginModel () {
+    this.setState({
+      isLoginVisible: true
+    })
+  }
+
+  render () {
+    return (
+      <Row className={styles.sign}>
+        <Col offset={8} span={8}>
+          <Button onClick={this.showLoginModel} ghost>登陆</Button>
+          <LoginForm visible={this.state.isLoginVisible} onCancel={this.handleLoginCancel}/>
+        </Col>
+        <Col span={8}>
+          <Button ghost>注册</Button>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+const LoginForm = Form.create()(
+  ({visible, onCancel, onCreate, form}) => {
+    const { getFieldDecorator } = form
+    const formItemLayout = {
+      labelCol: { span: 4, offset: 1 },
+      wrapperCol: { span: 18 },
+    }
+    return (
+      <Modal
+        visible={visible}
+        title="登陆"
+        okText="登陆"
+        onCancel={onCancel}
+        onOk={onCreate}
+      >
+        <Form layout="vertical">
+          <FormItem {...formItemLayout} label="用户名">
+            {getFieldDecorator('title', {
+              rules: [{ required: true, message: '请输入用户名' }],
+            })(
+              <Input />
+            )}
+          </FormItem>
+          <FormItem {...formItemLayout} label="密码">
+            {getFieldDecorator('description', {
+              rules: [{required: true,  message: '请输入密码'}]
+            })(<Input type="password" />)}
+          </FormItem>
+        </Form>
+      </Modal>
+    )
+  }
 )
 
 export default HeaderNav
---
+++ src/components/LoginPanel.js
@@ -0,0 +1,46 @@
+import { Button, Modal, Form, Input, Radio } from 'antd'
+const FormItem = Form.Item
+
+
+
+class CollectionsPage extends React.Component {
+  state = {
+    visible: false,
+  };
+  showModal = () => {
+    this.setState({ visible: true });
+  }
+  handleCancel = () => {
+    this.setState({ visible: false });
+  }
+  handleCreate = () => {
+    const form = this.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+
+      console.log('Received values of form: ', values);
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  }
+  saveFormRef = (form) => {
+    this.form = form;
+  }
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>New Collection</Button>
+        <CollectionCreateForm
+          ref={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+        />
+      </div>
+    );
+  }
+}
+
+export default CollectionsPage
