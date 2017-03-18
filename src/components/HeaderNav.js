9c9d729fdfca595dab129aa17e8549a694bb91bf
Mondo <mondogao@gmail.com>
2017-03-18 14:52:40 +0800

    feat(components/signinout): 完成普通登陆基本样式

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Button, Menu, Row, Col, Modal, Form, Input, Radio } from 'antd'
+import { Menu, Row, Col, Form } from 'antd'
 import Container from 'components/ResponsiveContainer'
+import SignInOut from 'components/SignInOut'
 import styles from  './HeaderNav.pcss'
 
 const FormItem = Form.Item
@@ -24,76 +25,6 @@ const HeaderNav = ({menuData}) => (
   </Container>
 )
 
-class SignInOut extends React.Component{
-  constructor (props) {
-    super(props)
-    this.state = {
-      isLoginVisible: false
-    }
 
-    this.handleLoginCancel = this.handleLoginCancel.bind(this)
-    this.showLoginModel = this.showLoginModel.bind(this)
-  }
-
-  handleLoginCancel () {
-    this.setState({
-      isLoginVisible: false
-    })
-  }
-
-  showLoginModel () {
-    this.setState({
-      isLoginVisible: true
-    })
-  }
-
-  render () {
-    return (
-      <Row className={styles.sign}>
-        <Col offset={8} span={8}>
-          <Button onClick={this.showLoginModel} ghost>登陆</Button>
-          <LoginForm visible={this.state.isLoginVisible} onCancel={this.handleLoginCancel}/>
-        </Col>
-        <Col span={8}>
-          <Button ghost>注册</Button>
-        </Col>
-      </Row>
-    )
-  }
-}
-
-const LoginForm = Form.create()(
-  ({visible, onCancel, onCreate, form}) => {
-    const { getFieldDecorator } = form
-    const formItemLayout = {
-      labelCol: { span: 4, offset: 1 },
-      wrapperCol: { span: 18 },
-    }
-    return (
-      <Modal
-        visible={visible}
-        title="登陆"
-        okText="登陆"
-        onCancel={onCancel}
-        onOk={onCreate}
-      >
-        <Form layout="vertical">
-          <FormItem {...formItemLayout} label="用户名">
-            {getFieldDecorator('title', {
-              rules: [{ required: true, message: '请输入用户名' }],
-            })(
-              <Input />
-            )}
-          </FormItem>
-          <FormItem {...formItemLayout} label="密码">
-            {getFieldDecorator('description', {
-              rules: [{required: true,  message: '请输入密码'}]
-            })(<Input type="password" />)}
-          </FormItem>
-        </Form>
-      </Modal>
-    )
-  }
-)
 
 export default HeaderNav
---
+++ src/components/SignInOut.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import { Button, Row, Col, Modal, Form, Input, Tabs, Checkbox } from 'antd'
+const FormItem = Form.Item
+
+import styles from './SignInOut.pcss'
+
+class SignInOut extends React.Component{
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoginVisible: false
+    }
+
+    this.handleLoginCancel = this.handleLoginCancel.bind(this)
+    this.showLoginModel = this.showLoginModel.bind(this)
+  }
+
+  handleLoginCancel () {
+    this.setState({
+      isLoginVisible: false
+    })
+  }
+
+  showLoginModel () {
+    this.setState({
+      isLoginVisible: true
+    })
+  }
+
+  render () {
+    return (
+      <Row className={styles.sign}>
+        <Col offset={8} span={8}>
+          <Button onClick={this.showLoginModel} ghost>登陆</Button>
+          <LoginForm
+            visible={this.state.isLoginVisible}
+            onCancel={this.handleLoginCancel}
+          />
+        </Col>
+        <Col span={8}>
+          <Button ghost>注册</Button>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+const LoginForm = Form.create()(
+  ({visible, onCancel, onCreate, form}) => {
+    const { getFieldDecorator } = form
+    const formItemLayout = {
+      labelCol: { span: 4, offset: 3 },
+      wrapperCol: { span: 14 },
+    }
+    return (
+      <Modal
+        className={styles['login-panel']}
+        visible={visible}
+        title="登陆"
+        okText="登陆"
+        onCancel={onCancel}
+        onOk={onCreate}
+      >
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="账号登录" key="1" style={{padding: 16}}>
+            <Form layout="vertical">
+              <FormItem {...formItemLayout} label="用户名">
+                {getFieldDecorator('username', {
+                  rules: [{ required: true, message: '请输入用户名' }],
+                })(
+                  <Input />
+                )}
+              </FormItem>
+              <FormItem {...formItemLayout} label="密码">
+                {getFieldDecorator('password', {
+                  rules: [{required: true,  message: '请输入密码'}]
+                })(<Input type="password" />)}
+              </FormItem>
+              <FormItem {...formItemLayout} label=" ">
+                {getFieldDecorator('remember')(<Checkbox>记住密码</Checkbox>)}
+              </FormItem>
+            </Form>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="第三方登录" key="2"></Tabs.TabPane>
+        </Tabs>
+      </Modal>
+    )
+  }
+)
+
+export default SignInOut
