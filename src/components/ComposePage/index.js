48a9575de6e2373aa060fcc3f501a96d97b31e48
Mondo <mondogao@gmail.com>
2017-04-17 21:21:48 +0800

    feat(components/ComposePage): 基本完成样式

--- src/components/ComposePage/index.js
+++ src/components/ComposePage/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Row, Col, Input } from 'antd'
+import { Layout, Row, Col, Input, Button } from 'antd'
 
 import styles from './ComposePage.scss'
 
@@ -11,13 +11,14 @@ const ComposePage = ({}) => (
   <div>
     <Container className={styles['compose-container']}>
       <SearchInput/>
-      <EntityList entityIds={[1,2,3]} entity={TemplateItemContainer}/>
+      <EntityList className={styles.list} entityIds={[1,2,3]} entity={TemplateItemContainer}/>
     </Container>
+    <NoFitTip/>
   </div>
 )
 
 class SearchInput extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       value: ''
@@ -28,18 +29,37 @@ class SearchInput extends React.Component {
     this.setState({value: e.target.value})
   }
   
-  render () {
+  render() {
     return (
-    <div className={styles['search-input']}>
-      <Input.Search size="large" placeholder={"输入学校名称查找相应模板"} value={this.state.value} onChange={this.handleChange}/>
-      <p>
-        <span>国家标准格式论文模板</span>
-        <span>华中科技大学硕士论文模板</span>
-        <span>华中科技大学博士论文模板</span>
-      </p>
+      <div className={styles['search-input']}>
+        <Input.Search size="large" placeholder={"输入学校名称查找相应模板"} value={this.state.value} onChange={this.handleChange}/>
+        <p>
+          <span>国家标准格式论文模板</span>
+          <span>华中科技大学硕士论文模板</span>
+          <span>华中科技大学博士论文模板</span>
+        </p>
+      </div>
+    )
+  }
+}
+
+import imgNoFitTip from 'assets/compose-no-fit-tip.png'
+
+const NoFitTip = () => {
+  return (
+    <div className={styles['no-fit-tip']}>
+      <Container className={styles.container}>
+        <div className={styles['img-wrapper']}>
+          <img src={imgNoFitTip} alt=""/>
+        </div>
+        <div className={styles['tip-wrapper']}>
+          <h2>模板都不合适？</h2>
+          <p>我们提供免费定制模板服务，只要你上传论文规范，通过后还将获得积分奖励！</p>
+          <Button>定制模板</Button>
+        </div>
+      </Container>
     </div>
   )
-  }
 }
 
 export default ComposePage
