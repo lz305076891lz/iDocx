e08f3de67e1aab6858cce178be6b782fc75615b6
Mondo <mondogao@gmail.com>
2017-04-30 14:05:26 +0800

    feat(components/UploadPage): 增加封面选择列表逻辑

--- src/components/ComposePage/UploadPage/index.js
+++ src/components/ComposePage/UploadPage/index.js
@@ -1,75 +1,94 @@
 import React from 'react'
-import { Icon, Upload, Button } from 'antd'
+import { Icon, Upload, Button, Select } from 'antd'
 import { Link } from 'react-router-dom'
+const Option = Select.Option
 
 import styles from './UploadPage.scss'
 
 /**
  * @todo Fake Data
  */
-const UploadPage = ({ template = {
-  id: 1,
-  title: '国家标准格式通用模板',
-  type: '硕士',
-  imgSrc: require('assets/home-carousel-page-1.png')
-} }) => {
-  return (
-    <div>
-      <div className={styles['seleted-template-tip']}>
-        <Icon type="check-circle" className={styles['icon']}/>
-        <span>已选模板：{template.title}</span>
-        <Link to="/compose">修改模板</Link>
+class UploadPage extends React.Component {
+  state = {
+    fileList: [],
+    coverList: []
+  }
+  
+  handleFileListChange = fileList => {
+    this.setState(prevState => ({
+      fileList,
+      coverList: prevState.coverList.filter(cover => {
+        return fileList.map(file => file.uid).includes(cover)
+      })
+    }))
+  }
+  
+  handleCoverSelectChange = value => {
+    this.setState({
+      coverList: value
+    })
+  }
+  
+  render() {
+    const template = {
+      id: 1,
+      title: '国家标准格式通用模板',
+      type: '硕士',
+      imgSrc: require('assets/home-carousel-page-1.png')
+    }
+    
+    return (
+      <div>
+        <div className={styles['seleted-template-tip']}>
+          <Icon type="check-circle" className={styles['icon']}/>
+          <span>已选模板：{template.title}</span>
+          <Link to="/compose">修改模板</Link>
+        </div>
+        <div className={styles['upload-file-container']}>
+          <FileUpload
+            handleFileListChange={this.handleFileListChange}
+            fileList={this.state.fileList}
+          />
+          <div className={styles['cover-select-list-container']}>
+            <span className={styles['tip']}>
+              选择需要生成封面的论文:
+              <span>不需生成封面，可跳过此步</span>
+            </span>
+            <Select
+              mode="multiple"
+              className={styles['cover-select-list']}
+              value={this.state.coverList}
+              onChange={this.handleCoverSelectChange}
+            >
+              {
+                this.state.fileList.filter(file => file.status === 'done')
+                  .map(file => <Option key={file.uid}>{file.name}</Option>)
+              }
+            </Select>
+          </div>
+        </div>
+        <div className={styles['cover-meta-container']}>
+          封面信息补充
+        </div>
       </div>
-      <div className={styles['upload-file-container']}>
-        <FileUpload/>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 class FileUpload extends React.Component {
-  state = {
-    fileList: [{
-      uid: -1,
-      name: 'xxx.png',
-      status: 'done',
-      url: 'http://www.baidu.com/xxx.png',
-    }],
-  }
   handleChange = (info) => {
-    let fileList = info.fileList;
+    let fileList = info.fileList
     
-    // 1. Limit the number of uploaded files
-    //    Only to show two recent uploaded files, and old ones will be replaced by the new
-    // fileList = fileList.slice(-2);
-    //
-    // // 2. read from response and show file link
-    // fileList = fileList.map((file) => {
-    //   if (file.response) {
-    //     // Component will show file.url as link
-    //     file.url = file.response.url;
-    //   }
-    //   return file;
-    // });
-    
-    // 3. filter successfully uploaded files according to response from server
-    // fileList = fileList.filter((file) => {
-    //   if (file.response) {
-    //     return file.response.status === 'success';
-    //   }
-    //   return true;
-    // });
-    
-    this.setState({ fileList });
+    this.props.handleFileListChange(fileList);
   }
   render() {
     const props = {
       action: '//jsonplaceholder.typicode.com/posts/',
       onChange: this.handleChange,
       multiple: true,
-      fileList: this.state.fileList
+      fileList: this.props.fileList
     }
-    const hasFile = this.state.fileList.length > 0
+    const hasFile = this.props.fileList.length > 0
     
     return (
       <div className={`${styles['upload-area']} ${hasFile ? styles['has-item'] : ''}`}>
