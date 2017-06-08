32626fec7d315d0c940a0e0f569ecd453f21c226
Mondo <mondogao@gmail.com>
2017-06-08 14:47:31 +0800

    refactor: 去除未实现的功能

--- src/sources/templates.js
+++ src/sources/templates.js
@@ -6,50 +6,74 @@ const defaultTemplates = [
   {
     id: 1,
     title: '国家标准格式通用模板1',
-    type: '硕士',
-    imgSrc: require('assets/home-carousel-page-1.png')
+    type: {
+      id: 1,
+      name: '硕士'
+    },
+    coverSrc: require('assets/home-carousel-page-1.png'),
   },
   {
     id: 2,
     title: '国家标准格式通用模板2',
-    type: '硕士',
-    imgSrc: require('assets/home-carousel-page-2.png')
+    type: {
+      id: 1,
+      name: '硕士'
+    },
+    coverSrc: require('assets/home-carousel-page-2.png')
   },
   {
     id: 3,
     title: '国家标准格式通用模板3',
-    type: '硕士',
-    imgSrc: require('assets/home-carousel-page-1.png')
+    type: {
+      id: 1,
+      name: '硕士'
+    },
+    coverSrc: require('assets/home-carousel-page-1.png')
   },
   {
     id: 4,
     title: '国家标准格式通用模板4',
-    type: '硕士',
-    imgSrc: require('assets/home-carousel-page-2.png')
+    type: {
+      id: 1,
+      name: '硕士'
+    },
+    coverSrc: require('assets/home-carousel-page-2.png')
   },
   {
     id: 5,
     title: '国家标准格式通用模板5',
-    type: '硕士',
-    imgSrc: require('assets/home-carousel-page-1.png')
+    type: {
+      id: 1,
+      name: '硕士'
+    },
+    coverSrc: require('assets/home-carousel-page-1.png')
   },
   {
     id: 6,
     title: '国家标准格式通用模板6',
-    type: '硕士',
-    imgSrc: require('assets/home-carousel-page-2.png')
+    type: {
+      id: 1,
+      name: '硕士'
+    },
+    coverSrc: require('assets/home-carousel-page-2.png')
   },
   {
     id: 7,
     title: '国家标准格式通用模板7',
-    type: '硕士',
-    imgSrc: require('assets/home-carousel-page-1.png')
+    type: {
+      id: 1,
+      name: '硕士'
+    },
+    coverSrc: require('assets/home-carousel-page-1.png')
   },
   {
     id: 8,
     title: '国家标准格式通用模板8',
-    type: '硕士',
-    imgSrc: require('assets/home-carousel-page-2.png')
+    type: {
+      id: 1,
+      name: '硕士'
+    },
+    coverSrc: require('assets/home-carousel-page-2.png')
   }
 ]
 
