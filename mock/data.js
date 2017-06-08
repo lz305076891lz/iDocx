f6a52a6dcd19375d6502b80b047c55294b2679f2
Mondo <mondogao@gmail.com>
2017-06-08 16:11:07 +0800

    mock(templates): 完成 api

---
+++ mock/data.js
@@ -0,0 +1,40 @@
+module.exports = {
+  templates: [
+    {
+      id: "1",
+      title: "华中科技大学本科论文",
+      type: {
+        id: "string",
+        name: "本科论文"
+      },
+      coverSrc: "/api/statics/cover.png",
+      tags: [
+        "string"
+      ]
+    },
+    {
+      id: "2",
+      title: "华中科技大学硕士论文",
+      type: {
+        id: "string",
+        name: "硕士论文"
+      },
+      coverSrc: "/api/statics/cover.png",
+      tags: [
+        "string"
+      ]
+    },
+    {
+      id: "3",
+      title: "律师事务",
+      type: {
+        id: "string",
+        name: "法律文章"
+      },
+      coverSrc: "/api/statics/cover.png",
+      tags: [
+        "string"
+      ]
+    },
+  ]
+}
