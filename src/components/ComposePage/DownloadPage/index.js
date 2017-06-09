8371f662869c9b2bc1c635026fa4e48d6c43c9dc
Mondo <mondogao@gmail.com>
2017-06-10 00:28:11 +0800

    feat(components/DownloadPage): 完善流程，增加功能

--- src/components/ComposePage/DownloadPage/index.js
+++ src/components/ComposePage/DownloadPage/index.js
@@ -2,54 +2,81 @@ import React from 'react'
 import { Row, Col, Card, Select, Button, Tabs } from 'antd'
 const Option = Select.Option
 const TabPane = Tabs.TabPane
+import { Redirect } from 'react-router-dom'
 
 import styles from './DownloadPage.scss'
 
 import InFlowTip from 'components/InFlowTip'
 
-const DownloadPage = ({ fishList, fishes }) => {
-  return (
-    <div className={styles['download-page']}>
-      <InFlowTip
-        tip="排版成功！"
-        linkTo="/compose/upload"
-        linkText="重新上传"/>
-      <Row gutter={36}>
-        <Col span={6}>
-          <div className={styles['wrapper']}>
-            <Card title="下载">
-              <Select placeholder="请选择下载版本">
-                {Object.keys(fishes[0].downloadLinks).map(typeName => {
-                  let type = fishes[0].downloadLinks[typeName]
-                  
-                  return <Option value={type.id} key={type.id}>{type.name}</Option>
-                })}
-              </Select>
-              <Button type="primary" className={styles['btn-download']} onClick={e => {
-                
-              }}>
-                下载
-              </Button>
-            </Card>
-          </div>
-        </Col>
+class DownloadPage extends React.Component{
+  state = {
+    downloadType: undefined
+  }
   
-        <Col className={styles['preview-container']} span={18}>
-          <div className={styles['wrapper']}>
-            <Tabs>
-              {fishes.map(fish => (
-                <TabPane tab={fish.fileName} key={fish.id}>
-                  <div className={styles['preview-wrapper']}>
-                    <iframe src={fish.previewHref} frameBorder="0"/>
-                  </div>
-                </TabPane>
-              ))}
-            </Tabs>
-          </div>
-        </Col>
-      </Row>
-    </div>
-  )
+  handleSelectChange = value => {
+    this.setState({
+      downloadType: value
+    })
+  }
+  
+  handleDownloadButtonCllick = e => {
+    this.props.fishes.forEach(fish => {
+      window.open(fish.downloadLinks[this.state.downloadType].downloadLink)
+    })
+  }
+  
+  render() {
+    if (this.props.fishes.length < 1) {
+      return (
+        <Redirect to={`/compose`}/>
+      )
+    }
+    
+    return (
+      <div className={styles['download-page']}>
+        <InFlowTip
+          tip="排版成功！"
+          linkTo="/compose/upload"
+          linkText="重新上传"/>
+        <Row gutter={36}>
+          <Col span={6}>
+            <div className={styles['wrapper']}>
+              <Card title="下载">
+                <Select placeholder="请选择下载版本" value={this.state.downloadType} onChange={this.handleSelectChange}>
+                  {Object.keys(this.props.fishes[0].downloadLinks).map(typeName => {
+                    let type = this.props.fishes[0].downloadLinks[typeName]
+                  
+                    return <Option value={typeName} key={type.id}>{type.name}</Option>
+                  })}
+                </Select>
+                <Button
+                  type="primary"
+                  className={styles['btn-download']}
+                  onClick={this.handleDownloadButtonCllick}
+                  disabled={!this.state.downloadType}>
+                  下载
+                </Button>
+              </Card>
+            </div>
+          </Col>
+        
+          <Col className={styles['preview-container']} span={18}>
+            <div className={styles['wrapper']}>
+              <Tabs>
+                {this.props.fishes.map(fish => (
+                  <TabPane tab={fish.fileName} key={fish.id}>
+                    <div className={styles['preview-wrapper']}>
+                      <iframe src={fish.previewHref} frameBorder="0"/>
+                    </div>
+                  </TabPane>
+                ))}
+              </Tabs>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
 }
 
 import { connect } from 'react-redux'
