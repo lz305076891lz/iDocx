3737ede40b5721c20f43af4e18375def9fd1ade3
Mondo <mondogao@gmail.com>
2017-09-15 21:46:17 +0800

    feat(SignInOut): 完成表单显示逻辑

--- src/components/SignInOut/index.js
+++ src/components/SignInOut/index.js
@@ -1,69 +1,81 @@
 import React from 'react';
+import cn from 'classnames';
 import { Button, Row, Col, Modal, Form, Input, Tabs, Checkbox } from 'antd';
 
 import styles from './SignInOut.scss';
 
 const FormItem = Form.Item;
 
+const FORM_STATUS = {
+  SIGNUP: 'SIGNUP',
+  LOGIN: 'LOGIN',
+};
+
 class SignInOut extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoginVisible: false,
-    };
+   state = {
+     isModalVisible: false,
+     isModalLoading: false,
+     formStatus: FORM_STATUS.LOGIN,
+   }
 
-    this.handleLoginCancel = this.handleLoginCancel.bind(this);
-    this.showLoginModel = this.showLoginModel.bind(this);
-  }
 
-  handleLoginCancel() {
+  handleCancel = () => {
     this.setState({
-      isLoginVisible: false,
+      isModalVisible: false,
     });
   }
 
-  showLoginModel() {
+  handleSubmit = (values) => {
+    if (this.state.isModelLoading) {
+      return;
+    }
     this.setState({
-      isLoginVisible: true,
+      isModalLoading: true,
+    });
+
+    console.log(values);
+  }
+
+  showModel = (formStatus = FORM_STATUS.LOGIN) => () => {
+    this.setState({
+      isModalVisible: true,
+    });
+
+    this.handleTabChange(formStatus);
+  }
+
+  handleTabChange = (nextStatus) => {
+    if (this.state.isModalLoading) {
+      return;
+    }
+
+    this.setState({
+      formStatus: nextStatus,
     });
   }
 
   render() {
-    const { isLoginVisible } = this.state;
+    const { isModalVisible, isModalLoading, formStatus } = this.state;
 
 
     return (
-      <Row className={`${styles.sign} ${this.props.isTransparent ? styles['sign-transparent'] : ''}`}>
+      <Row className={cn(styles.sign, {
+          [styles['sign-transparent']]: this.props.isTransparent,
+        })}>
         <Col offset={8} span={8}>
-          <Button onClick={this.showLoginModel} ghost>登陆</Button>
+          <Button onClick={this.showModel()} ghost>登陆</Button>
         </Col>
         <Col span={8}>
-          <Button ghost>注册</Button>
+          <Button onClick={this.showModel(FORM_STATUS.SIGNUP)} ghost>注册</Button>
         </Col>
-        <Modal
-          className={styles['login-panel']}
-          visible={isLoginVisible}
-          title="登陆"
-          okText="登陆"
-          onCancel={this.handleLoginCancel}
-          onOk={this.handleLoginCancel}
-        >
-          <Tabs defaultActiveKey="login">
-            <Tabs.TabPane tab="登录" key="login">
-              <LoginForm/>
-            </Tabs.TabPane>
-            <Tabs.TabPane tab="注册" key="signup">
-              <LoginForm/>
-            </Tabs.TabPane>
-          </Tabs>
-        </Modal>
+        <LoginForm activeKey={formStatus} isVisible={isModalVisible} onCancel={this.handleCancel} onSubmit={this.handleSubmit} onTabChange={this.handleTabChange} isLoading={isModalLoading}/>
       </Row>
     );
   }
 }
 
 const LoginForm = Form.create()(({
-  form,
+  form, onCancel, isVisible, onSubmit, activeKey, onTabChange, isLoading
 }) => {
   const { getFieldDecorator } = form;
   const formItemLayout = {
@@ -71,22 +83,51 @@ const LoginForm = Form.create()(({
     wrapperCol: { span: 14 },
   };
 
+  const handleSubmit = () => form.validateFields((errs, values) => {
+    if (errs) {
+      return;
+    }
+
+    onSubmit(values);
+  });
+
   return (
-    <Form layout="vertical" style={{ paddingTop: 16 }}>
-      <FormItem {...formItemLayout} label="用户名">
-        {getFieldDecorator('username', {
-           rules: [{ required: true, message: '请输入用户名' }],
-        })(<Input />)}
-      </FormItem>
-      <FormItem {...formItemLayout} label="密码">
-        {getFieldDecorator('password', {
-           rules: [{ required: true, message: '请输入密码' }],
-        })(<Input type="password" />)}
-      </FormItem>
-      <FormItem {...formItemLayout} label=" ">
-        {getFieldDecorator('remember')(<Checkbox>记住密码</Checkbox>)}
-      </FormItem>
-    </Form>
+    <Modal
+      className={styles['login-panel']}
+      visible={isVisible}
+      title="登陆"
+      okText="登陆"
+      confirmLoading={isLoading}
+      onCancel={onCancel}
+      onOk={handleSubmit}
+    >
+      <Tabs activeKey={activeKey} onChange={onTabChange}>
+        <Tabs.TabPane tab="登录" key={FORM_STATUS.LOGIN}/>
+        <Tabs.TabPane tab="注册" key={FORM_STATUS.SIGNUP}/>
+      </Tabs>
+      <Form layout="vertical" style={{ paddingTop: 16 }}>
+        <FormItem {...formItemLayout} label="手机号">
+          {getFieldDecorator('tel', {
+             rules: [{ required: true, message: '请输入手机号' }],
+          })(<Input/>)}
+        </FormItem>
+        {activeKey === FORM_STATUS.SIGNUP ?
+         <FormItem {...formItemLayout} label="用户名">
+           {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入用户名' }],
+           })(<Input/>)}
+         </FormItem> : null}
+        <FormItem {...formItemLayout} label="密码">
+          {getFieldDecorator('password', {
+             rules: [{ required: true, message: '请输入密码' }],
+          })(<Input type="password"/>)}
+        </FormItem>
+        {activeKey === FORM_STATUS.LOGIN ?
+         <FormItem {...formItemLayout} label=" ">
+           {getFieldDecorator('remember')(<Checkbox>记住密码</Checkbox>)}
+         </FormItem> : null}
+      </Form>
+    </Modal>
   );
 });
 
