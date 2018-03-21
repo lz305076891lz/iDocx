86edba213fd22894e3064d8f615ad2e42a689ae4
Mondo <mondogao@gmail.com>
2018-03-21 22:40:29 +0800

    feat(UploadTemplate): add Upload component

--- src/components/UploadTemplate/index.js
+++ src/components/UploadTemplate/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Upload, Icon } from 'antd';
+
+import styles from './UploadTemplate.scss';
+
+const { Dragger } = Upload;
 
 function UploadTemplate() {
-  return 'uploadtempalte';
+  return (
+    <Dragger className={styles.uploadArea}>
+      <p>
+        <Icon type="inbox" className={styles.icon}/>
+      </p>
+      <h4>拖拽或单击上传</h4>
+    </Dragger>
+  )
 }
 
 export default connect()(UploadTemplate);
