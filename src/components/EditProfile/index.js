e26c5ab355ca1e3d07cab995316f7502861d748a
Mondo <mondogao@gmail.com>
2018-03-26 22:15:59 +0800

    feat(EditProfile): finish form structure

--- src/components/EditProfile/index.js
+++ src/components/EditProfile/index.js
@@ -10,8 +10,13 @@ import styles from './EditProfile.scss';
 
 const formItemLayout = {
   labelCol: { span: 4 },
-  wrapperCol: { span: 8 },
+  wrapperCol: { span: 16 },
 };
+const formTailLayout = {
+  labelCol: { span: 4 },
+  wrapperCol: { offset: 4, span: 12 },
+};
+
 
 export function ProfileItem({ title, value }) {
   return (
@@ -42,26 +47,75 @@ export function ShowProfile({ user, onSubmit }) {
   )
 }
 
-@Form.create({
-  mapPropsToFields(props) {
-    return map(compose(Form.createFormField, objOf('value')), props.fields);
-  },
-  onValuesChange(props, values) {
-    props.onChange(values);
-  }
-})
+@Form.create()
 export class EditProfileForm extends React.Component {
-  handleSubmit = () => {
-    this.props.onSubmit();
+  state = {
+    isTelChanged: false,
+    defaultValue: this.props.user,
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if(!err) {
+        console.log(values);
+        this.props.onSubmit(values);
+      }
+    })
+  }
+
+  handleTelChange = () => {
+    this.setState(() => ({
+      isTelChanged: true,
+    }));
+  }
+
+  renderPhoneCode = () => {
+    if (!this.state.isTelChanged) {
+      return null;
+    }
+
+    return (
+      <Form.Item {...formItemLayout} label="验证码">
+        {this.props.form.getFieldDecorator('smscode')(
+          <Row gutter={4}>
+            <Col span={16}>
+              <Input />
+            </Col>
+            <Col span={8}>
+              <Button className={styles.btnSms}>获取验证码</Button>
+            </Col>
+          </Row>
+        )}
+      </Form.Item>
+    )
   }
 
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const { user, form } = this.props;
+    const { getFieldDecorator } = form;
 
     return (
-      <Form layout="vertical">
+      <Form layout="vertical" onSubmit={this.handleSubmit}>
         <Form.Item {...formItemLayout} label="用户名">
-          {getFieldDecorator('username')(<Input />)}
+          {getFieldDecorator('username', {
+            initialValue: user.username,
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item {...formItemLayout} label="手机号">
+          {getFieldDecorator('tel', {
+            initialValue: user.tel,
+          })(<Input onChange={this.handleTelChange} />)}
+        </Form.Item>
+        {this.renderPhoneCode()}
+        <Form.Item {...formItemLayout} label="Email">
+          {getFieldDecorator('email', {
+            initialValue: user.email,
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item {...formTailLayout}>
+          <Button htmlType="submit">提交</Button>
         </Form.Item>
       </Form>
     );
@@ -79,7 +133,6 @@ export default class EditProfile extends React.Component {
 
   state = {
     status: EditProfile.Status.view,
-    fields: this.props.user,
   }
 
   isEditable = () => this.state.status === EditProfile.Status.edit
@@ -95,7 +148,11 @@ export default class EditProfile extends React.Component {
     const { user } = this.props;
 
     const main = this.isEditable() ?
-      <EditProfileForm onSubmit={this.toggleStatus} fields={fields} onChange={this.handleFormChange} /> :
+      <EditProfileForm
+        onSubmit={this.toggleStatus}
+        fields={fields}
+        onChange={this.handleFormChange}
+        user={user} /> :
       <ShowProfile user={user} onSubmit={this.toggleStatus} />;
 
     return (
